790b9c7d6ece29c9b4b6266ced583c0c247c41d0
XeeroSs <qmasini@gmail.com>
2019-11-02 20:27:22 +0100

    Add Androidx

--- app/src/main/java/com/app/xeross/infoxer/Model/ClassementAdapter.java
+++ app/src/main/java/com/app/xeross/infoxer/Model/ClassementAdapter.java
@@ -6,8 +6,8 @@ package com.app.xeross.infoxer.Model;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.support.v7.widget.PopupMenu;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.View;
